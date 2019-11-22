9ee08e045fe0d4f253e52dfd0b1960488caae399
Myron <dubtwee@gmail.com>
2019-11-22 12:30:21 -0600

    adding a render for the retrieved account balance to be displayed to the user

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -16,10 +16,19 @@ function getAccountBalance() {
     .then(function(response) {
         // successful GET response
         console.log('response: ', response);
+        render(response);
     })
     .catch(function(err) {
         console.log('err: ', err);
         // error from GET response
         alert('There was an error retrieving your account balance.');
     });
+}
+
+function render(account) {
+    const $balance = $('.js-balance');
+
+    $balance.append(`
+        <strong>$${account.balance}</strong>
+    `);
 }
